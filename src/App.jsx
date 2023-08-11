14e2399c849001680c15d1bc61422e3aa8a24761
DinnyTech <77016654+Danobentey@users.noreply.github.com>
2023-08-11 23:08:44 +0100

    initial templete

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelContainer, ChannelListContainaer } from './components';
+
+import './App.css'
+
+const apiKey = 's2cyj2m6sake';
+
+const client = StreamChat.getInstance('apiKey')
+
+const App = () => { 
+  return (
+    <div>
+      <div className="app__wrapper">
+        <Chat client={client} theme='team light'>
+          <ChannelListContainaer />
+
+          <ChannelContainer />
+        </Chat>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as ChannelContainer } from './ChannelContainer';
+export { default as ChannelListContainaer } from './ChannelListContainer';
+export { default as ChannelSearch } from './ChannelSearch';
